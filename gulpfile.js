c55e1342ec1ee5ab9bb9a274668b2d3a68fb613a
WartClaes <wart.claes@gmail.com>
2016-08-30 14:56:57 +0200

    added more color variables, added scss linting

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var pug = require('gulp-pug');
 var rimraf = require('rimraf');
 var runSequence = require('run-sequence');
 var sass = require('gulp-sass');
+var sassLint = require('gulp-sass-lint');
 var webserver = require('gulp-webserver');
 
 var config = {
@@ -40,21 +41,30 @@ gulp.task('views', function(){
 
 gulp.task('styles', function(){
   return gulp.src(config.styles.demo)
+    .pipe(sassLint())
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest(config.styles.dest));
 });
 
+gulp.task('sass-lint', function () {
+  return gulp.src(config.styles.src)
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+});
+
 gulp.task('watch', function(){
   gulp.watch(config.views.src, ['views']);
-  gulp.watch(config.styles.src, ['styles']);
+  gulp.watch(config.styles.src, ['sass-lint','styles']);
 
   gulp.watch(config.views.demo, ['views']);
-  gulp.watch(config.styles.demo, ['styles']);
+  gulp.watch(config.styles.demo, ['sass-lint', 'styles']);
 });
 
 gulp.task('default', function(cb){
   runSequence(
     'clean',
+    'sass-lint',
     ['views', 'styles'],
     'webserver',
     'watch',
